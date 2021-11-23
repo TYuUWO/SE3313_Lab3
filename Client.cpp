82573c739e38ea304e42a60de237eb371af5c9b3
TYuUWO <tyu263@uwo.ca>
2021-11-22 21:54:24 -0500

    client

--- Client.cpp
+++ Client.cpp
@@ -4,21 +4,53 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <string>
 
 using namespace Sync;
+using namespace std;
 
 int main(void)
 {
+	string inputData;
+	bool socketCreated = true;
+	int socketReturn;
 	// Welcome the user 
 	std::cout << "SE3313 Lab 3 Client" << std::endl;
-
+	
 	// Create our socket
 	Socket socket("127.0.0.1", 3000);
-	//To write to socket and read from socket. You may use ByteArray 
-	//socket.Write(data);
-	//socket.Read(data);
+	//attempt to initiate connection
+	//if socket creation failed, set the flag and print the error
+	try {socket.Open();}
+	catch (...){
+		socketCreated = false;
+		cout << "Socket cannot be created" <<endl;
+	}
+	
+	//main program loop
+	while(inputData!="done" && socketCreated){
+		std::cin >> inputData;
+	
+		//convert inputData to ByteArray data
+		ByteArray data(inputData);
 
+		//To write to socket and read from socket. You may use ByteArray 
+		socket.Write(data);
+		socket.Read(data);
+		
+		//end server connection and exit based on server reply
+		if (data.ToString() == "exit" || stoi(data.ToString()) == -1){
+			cout << "Server connection terminated" <<endl;
+			break;
+		}
+		
+		
+		//print the received data
+		std::cout << data.ToString() <<endl;
+	}
+	
+	//close the socket
 	socket.Close();
-
+	
 	return 0;
 }
